ce63cef3823448b9b3c74a6840af378d204f754e
TranVietThuc <cnttitk36@gmail.com>
2025-03-31 19:44:29 +0700

    update

--- webRTC/js/constWebRTC.js
+++ webRTC/js/constWebRTC.js
@@ -1,19 +1,31 @@
-﻿
 const peerConnectionConfig = {
     "iceServers": [
-        { "urls": "stun:hk-turn1.xirsys.com" },
+        // { "urls": "stun:hk-turn1.xirsys.com" },
+        // {
+        //     "urls": [
+        //         "turn:hk-turn1.xirsys.com:80?transport=udp",
+        //         "turn:hk-turn1.xirsys.com:3478?transport=udp",
+        //         "turn:hk-turn1.xirsys.com:80?transport=tcp",
+        //         "turn:hk-turn1.xirsys.com:3478?transport=tcp",
+        //         "turns:hk-turn1.xirsys.com:443?transport=tcp",
+        //         "turns:hk-turn1.xirsys.com:5349?transport=tcp"
+        //     ],
+        //     "username": "6uAzOTFiPLpbQASUhGwRb90arFmWB0NpVN6YX_LnQ-RRk3dyMgMz_MRNn9JJXhsQAAAAAGfqYU5tZXNocm9vbQ==",
+        //     "credential": "24501b26-0e13-11f0-a4b9-0242ac120004",
+        // },
+        //
+        // { "urls": "stun:freestun.net:3478" },
+        // {
+        //     "urls": "turn:freestun.net:3478",
+        //     "username": "free",
+        //     "credential": "free"
+        // }
+        //
         {
-            "urls": [
-                "turn:hk-turn1.xirsys.com:80?transport=udp",
-                "turn:hk-turn1.xirsys.com:3478?transport=udp",
-                "turn:hk-turn1.xirsys.com:80?transport=tcp",
-                "turn:hk-turn1.xirsys.com:3478?transport=tcp",
-                "turns:hk-turn1.xirsys.com:443?transport=tcp",
-                "turns:hk-turn1.xirsys.com:5349?transport=tcp"
-            ],
-            "username": "z9TpC1KPfzniEVLyzf1PBsScx8n96fIyfwgQmxm1fnbh4ymIle2mBR1BNtjgCyxDAAAAAGfpGoZreW9seQ==",
-            "credential": "5da90892-0d50-11f0-9795-0242ac120004",
-        },
+            "url": "turn:117.3.248.224:443?transport=udp",
+            "username": "thiagoTeams",
+            "credential": "Abcd@1234"
+        }
     ]
 }
 var domainLive = `${window.location.protocol}//${window.location.hostname}${(window.location.port ? ':' + window.location.port : ':7111')}`;
